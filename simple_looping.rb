e1ce69350a053f38c386981b9589e5d67057b8c3
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-04-09 17:39:34 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -60,10 +60,8 @@ def for_iterator(number_of_times)
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
-  counter = 0
-  for counter in number_of_times
+  for count in 1..number_of_times
     puts phrase
-    counter += 1
   end
 
 end
